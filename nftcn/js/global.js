3b1874a2618ccd74c6b8fdaff83628320e559456
神之浩 <31089649+hao45e@users.noreply.github.com>
2023-01-29 14:30:14 +0800

    Update global.js

--- nftcn/js/global.js
+++ nftcn/js/global.js
@@ -1,10 +1,10 @@
 function initMenu(){
     let str = "<div style='text-align: center'>";
-    str += "<a href='/goods-info.html'><div class='btn btn-primary'>藏品信息</div></a>";
-    str += "<a href='/add-goods.html'><div class='btn btn-primary'>增加藏品</div></a>";
-    str += "<a href='/add-relationship.html'><div class='btn btn-primary'>藏品关联</div></a>";
-    str += "<a href='/show/graph.html'><div class='btn btn-primary'>关联展示</div></a>";
-    str += "<a href='/static-js.html'><div class='btn btn-primary'>公共js</div></a>";
-    str += "<a href='/statistics-position.html'><div class='btn btn-primary'>持仓统计</div></a>";
+//     str += "<a href='/goods-info.html'><div class='btn btn-primary'>藏品信息</div></a>";
+//     str += "<a href='/add-goods.html'><div class='btn btn-primary'>增加藏品</div></a>";
+//     str += "<a href='/add-relationship.html'><div class='btn btn-primary'>藏品关联</div></a>";
+//     str += "<a href='/show/graph.html'><div class='btn btn-primary'>关联展示</div></a>";
+    str += "<a href='/nftcn/static-js.html'><div class='btn btn-primary'>公共js</div></a>";
+    str += "<a href='/nftcn/statistics-position.html'><div class='btn btn-primary'>持仓统计</div></a>";
     $("body").prepend(str+"</div>");
 }
